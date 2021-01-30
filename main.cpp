5becd8d2366c5ff7b22f1adc5a5297ef0196560b
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-01-30 14:33:37 -0700

    I may have it

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include<string>
+
 using namespace std;
+
 int main(int argc, char* argv[])
 {
     string fileName = argv[1];
-    ifstream input(fileName);
-    cout << fileName;
-    char out[input.gcount()];
-    cout << out;
-    input.read(out, input.gcount());
-    cout << out[1]+out[2]+out[3];
+    ifstream input;
+    string read;
+    string file;
+
+    input.open(fileName);
+    if (input.fail())
+    {
+        cerr << "File Read Error" << endl;
+        exit(1);
+    }
+    else
+    {
+        for (int i = 0; !input.eof(); i++)
+        {
+            input >> read;
+            cout << read << endl;
+            file = file + read;
+        }
+        cout << file << endl;
+    }
+    input.close();
     return 0;
 }
