1976942f57b7b382d36385f610e93b2680788634
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-01-30 12:03:24 -0700

    debug again

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char* argv[])
 {
     string fileName = argv[1];
     ifstream input(fileName);
-    string out;
-    input >> out;
+    string out((std::istreambuf_iterator<char>(input)),
+        (std::istreambuf_iterator<char>()));
     cout << out;
     return 0;
 }
