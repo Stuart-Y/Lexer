6579a2231109dc6810d00bc0bd443d19679bec33
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-01-30 12:15:08 -0700

    come on

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char* argv[])
 {
     string fileName = argv[1];
     ifstream input(fileName);
+    cout << fileName;
     string out = "whoops";
+    cout << out;
     out.assign((istreambuf_iterator<char>(input)),
         (istreambuf_iterator<char>()));
     cout << out;
