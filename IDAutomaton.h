35c57b99e69632895b204299b22e97b699881fad
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-02 15:08:09 -0700

    debug

--- IDAutomaton.h
+++ IDAutomaton.h
@@ -16,10 +16,9 @@ public:
         maxReadCount = 0;
        if (isalpha(input[0]) == 0)
         {
-           cout << isalpha(input[0]) << "if" << '\n';
            for (unsigned int i = 0; isalpha(input[i]) == 0; i++)
            {
-               cout << isalpha(input[i]) << "for" << '\n';
+               cout << input[i] << '\n';
                maxReadCount++;
            }
            cout << maxReadCount << '\n';
