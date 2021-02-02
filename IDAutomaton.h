c6a5b48ca88bae95f1de9f4b19053708d8521025
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-02 15:39:45 -0700

    wrong edit

--- IDAutomaton.h
+++ IDAutomaton.h
@@ -14,9 +14,9 @@ public:
     int Start(string input, int lineNumber)
     {
         maxReadCount = 0;
-       if (isalnum(input[0]) != 0)
+       if (isalpha(input[0]) != 0)
         {
-           for (unsigned int i = 0; isalpha(input[i]) != 0; i++)
+           for (unsigned int i = 0; isalnum(input[i]) != 0; i++)
            {
                maxReadCount++;
            }
