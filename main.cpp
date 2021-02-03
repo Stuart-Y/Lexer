2ce2a842a725a534c6204268dc083f0d6613e905
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-02 19:40:07 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include<string>
+#include "Lexer.h"
+#include "Token.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string fileName = argv[1];
+    ifstream input;
+    string file;
+    vector <Token*> out;
+
+    input.open(fileName);
+    if (input.fail())
+    {
+        cerr << "File Read Error" << endl;
+        exit(1);
+    }
+    else
+    {
+        for (int i = 0; input.peek() != EOF; i++)
+        {
+            file = file += input.get();
+        }
+    }
+    input.close();
+    Lexer read;
+    out = read.Run(file);
+    for (unsigned int i = 0; i < out.size(); i++)
+    {
+        cout << out[i]->toString();
+    }
+    cout << "Total Tokens = " << out.size() << '\n';
+    return 0;
+}
